a0500042a15e293894796669db07c8effbd04c41
zakaria <zakaria.kais11@gmail.com>
2022-06-03 00:44:47 +0100

    redirecting attributes for creating a project now works

--- src/main/java/com/myProject/projectManagementSystem/controllers/ProjectController.java
+++ src/main/java/com/myProject/projectManagementSystem/controllers/ProjectController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.myProject.projectManagementSystem.models.Developer;
 import com.myProject.projectManagementSystem.models.Project;
@@ -33,17 +34,21 @@ public class ProjectController {
 	@Autowired
 	private DeveloperService developerService;
 	
-	@GetMapping("/projects-table")
-	public String getProjectsPage(Model model) {
-		List<Project> projects= projectService.getProjects();
-		model.addAttribute("projects",projects);
-		return "projects-table";
-	}
+	/******
+	 * 
+	 * Where to use redirect attributes
+	 * 
+	 * 
+	 * *****/
 	
 	@GetMapping("/add-project")
 	public String addProjectPage(Model model) {
 		
-		model.addAttribute("project", new Project());
+		
+		if (!model.containsAttribute("project")) {
+			model.addAttribute("project",new Project());
+	    }
+		
 		List<ProjectManager> projectManagers =  projectManagerService.getProjectManagers();//
 		List<Developer> allDevelopers =  developerService.getDevelopers();
 		List<Developer> developers = new ArrayList<Developer>();
@@ -58,43 +63,42 @@ public class ProjectController {
 	}
 	
 	
-	@GetMapping("/project-added")
-	public String addProgectGet() {
-		return "redirect:add-project";
-
-	}
 	
 	@PostMapping("/project-added")
-	public String addProject(@Valid @ModelAttribute("project") Project project,BindingResult bindingResult,Model model) {
+	public String addProject(@Valid @ModelAttribute("project") Project project,BindingResult bindingResult,RedirectAttributes redirectAttributes) {
 		
 		if(!bindingResult.hasErrors()) {
-			model.addAttribute("projectInserted",true);
+			redirectAttributes.addFlashAttribute("projectInserted",true);
 			project.setState("en cours");
 			projectService.addProject(project);
 			for(Developer developer : project.getDevelopers()) {
 				developer.setProject(project);
 				developerService.addDeveloper(developer);
 			}
-			Project project1 = new Project();
-			model.addAttribute("project",project1);
 		}else {
-			
-			model.addAttribute("projectNotInserted",true);
+			redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.project", bindingResult);
+		   
+			 redirectAttributes.addFlashAttribute("project",project); 
+			 redirectAttributes.addFlashAttribute("projectNotInserted",true);
 		}
-		//show developers and project managers dynamically
-		List<ProjectManager> projectManagers =  projectManagerService.getProjectManagers();//
-		List<Developer> allDevelopers =  developerService.getDevelopers();
-		List<Developer> developers = new ArrayList<Developer>();
-		for(Developer developer: allDevelopers) {
-			if(developer.getProject()==null) {
-				developers.add(developer);
-			}
-		}
-		model.addAttribute("developers", developers);
-
-		model.addAttribute("projectManagers", projectManagers);
-		return "add-project";
+		
+		return "redirect:add-project";
 	}
+	
+	/*********/
+	
+	
+
+	@GetMapping("/project-added")
+	public String addProgectGet() {
+		return "redirect:add-project";
+
+	}
+	
+	
+	
+	
+	
 	
 	@GetMapping("/delete-project")
 	public String deleteProject(@RequestParam int id) {
@@ -105,6 +109,15 @@ public class ProjectController {
 		}
 		projectService.deleteProject(id);
 		return "redirect:projects-table";
+	}
+	
+	
+	
+	@GetMapping("/projects-table")
+	public String getProjectsPage(Model model) {
+		List<Project> projects= projectService.getProjects();
+		model.addAttribute("projects",projects);
+		return "projects-table";
 	}
 
 	@GetMapping("/edit-project")
